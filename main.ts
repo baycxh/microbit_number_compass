2753be5ae2db0358ac38bbcd0776c641693e8932
baycxh <baycxh@yeah.net>
2023-04-15 20:58:22 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(9 * 3)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showNumber(18 + 5)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(30 / 6)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showNumber(9 - 8)
+})
